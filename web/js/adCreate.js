4dd477fdeba7550adbbbf7e04881ec6f6c6a56c0
1-stg <kabakov_glebochka@mail.ru>
2025-12-12 10:32:31 +0300

    add adCreate page

---
+++ web/js/adCreate.js
@@ -0,0 +1,6 @@
+let createButton = document.querySelector(`#buttonCreateAd`);
+
+createButton.addEventListener(`click`, () => {
+    alert(`Вы создали объявление`);
+    location.href = `index.html`;
+})
